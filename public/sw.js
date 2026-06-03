4b653c87bdb896ff8e16f144625e6e392665df39
chris0622ha <chris0622ha@gmail.com>
2026-06-03 19:27:10 -0400

    feat: FCM-enabled service worker

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,30 @@
-self.addEventListener("push", (event) => {
-  const data = event.data ? event.data.json() : {};
-  const title = data.title || "TrivQuic";
-  const options = {
-    body: data.body || "You have a new notification",
-    icon: "/icon-192.png",
-    badge: "/icon-192.png",
-    data: { url: data.url || "/" },
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "PLACEHOLDER_REPLACED_AT_RUNTIME",
+  authDomain: "onetap-trivia.firebaseapp.com",
+  databaseURL: "https://onetap-trivia-default-rtdb.firebaseio.com",
+  projectId: "onetap-trivia",
+  storageBucket: "onetap-trivia.firebasestorage.app",
+  messagingSenderId: "986046986694",
+  appId: "1:986046986694:web:2a4441bf46965ccbb3dac7",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const { title, body, url } = payload.data || {};
+  self.registration.showNotification(title || "TrivQuic", {
+    body: body || "You have a new notification",
+    icon: "/favicon.ico",
+    badge: "/favicon.ico",
+    data: { url: url || "/" },
     vibrate: [100, 50, 100],
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
+  });
 });
 
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data?.url || "/")
-  );
+  event.waitUntil(clients.openWindow(event.notification.data?.url || "/"));
 });
-
